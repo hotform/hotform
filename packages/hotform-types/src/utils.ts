0001838e56a78bd4583013368b421c09778c96b0
Derry Sucari <derrysucari01@gmail.com>
2023-02-08 19:13:14 -0500

    add UseStateWithCallbackLazyReturnType type

--- packages/hotform-types/src/utils.ts
+++ packages/hotform-types/src/utils.ts
@@ -1,3 +1,5 @@
 export type UseStateWithCallbackLazyCallback<T> = (value: T) => void;
 
 export type SetStateWithCallback<T> = (value: T, callback?: UseStateWithCallbackLazyCallback<T>) => void;
+
+export type UseStateWithCallbackLazyReturnType<T> = [ T, SetStateWithCallback<T> ];
