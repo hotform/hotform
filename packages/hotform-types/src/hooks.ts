fd5a0c8803774fd32ce46df8773ba254ef88ac80
Derry Sucari <derrysucari01@gmail.com>
2023-02-08 19:06:21 -0500

    add @hotform/types package code documentation

--- packages/hotform-types/src/hooks.ts
+++ packages/hotform-types/src/hooks.ts
@@ -4,20 +4,44 @@ import * as Schema from './schema';
 export type ResetHotFormSchema = () => void;
 
 export interface UseHotFormProps<T>{
+  /** If set to `true`, the field that changes its value will be validated. By default `true` is used. */
   hotField?: boolean;
+  
+  /** Initial hot form schema. */
   initialSchema: Schema.HotFormSchema<T>;
+  
+  /** The `onInvalid` callback is executed when the field values are invalid and the form is submitted. */
   onInvalid?: Events.HotFormFormValidityEventHandler<T>;
+  
+  /** The `onValid` callback is executed when the form is reset. */
   onReset?: Events.HotFormResetEventHandler<T>;
+  
+  /** The `onValid` callback is executed when the field values are valid and the form is submitted. */
   onValid?: Events.HotFormFormValidityEventHandler<T>;
-};
+}
 
 export interface UseHotFormReturnType<T>{
+  /** Current hot form schema. */
   currentSchema: Schema.HotFormSchema<T>;
-  handleBlur: React.FocusEventHandler;
+  
+  /** Hot form focus event handler. */
+  handleBlur: Events.HotFormFocusEventHandler;
+  
+  /** Hot form change event handler. */
   handleChange: Events.HotFormChangeEventHandler;
+  
+  /** Form reset event handler. */
   handleReset: React.FormEventHandler<HTMLFormElement>;
+  
+  /** Form submit event handler. */
   handleSubmit: React.FormEventHandler<HTMLFormElement>;
-  resetForm: ResetHotFormSchema;
-  setFieldValue: Schema.SetHotFormFieldValue<T>;
+  
+  /** Function to reset hot form schema. */
+  resetHotFormSchema: ResetHotFormSchema;
+  
+  /** Function to set hot form field value. */
+  setHotFormFieldValue: Schema.SetHotFormFieldValue<T>;
+  
+  /** `submitting` state. */
   submitting: boolean;
 }
